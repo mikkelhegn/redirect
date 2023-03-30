9355c4ebb3a62ef9ed620b0bfb4a696a344d3786
Mikkel Mørk Hegnhøj <mikkel@fermyon.com>
2023-03-30 17:17:19 +0200

    Spin app
    Signed-off-by: Mikkel Mørk Hegnhøj <mikkel@fermyon.com>

---
+++ api/src/lib.rs
@@ -0,0 +1,73 @@
+use anyhow::Result;
+use http::{Method, StatusCode};
+use rand::{distributions::Alphanumeric, Rng};
+use serde::{Deserialize, Serialize};
+use spin_sdk::{
+    http::{Request, Response},
+    http_component,
+    key_value::{Error, Store},
+};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Link {
+    name: String,
+    #[serde(default = "rand_string")]
+    short_url: String,
+    url: String,
+}
+
+fn rand_string() -> String {
+    return rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(8)
+        .map(char::from)
+        .collect();
+}
+
+/// A simple Spin HTTP component.
+#[http_component]
+fn handle_redirect(req: Request) -> Result<Response> {
+    let store = Store::open_default()?;
+
+    let (status, body) = match req.method() {
+        &Method::POST => {
+            let body = req.body().clone().unwrap();
+            let url_entry: Link = serde_json::from_slice(&body)?;
+
+            println!("{:?}", url_entry);
+
+            store.set(
+                &url_entry.short_url,
+                serde_json::to_vec(&url_entry.clone())?,
+            )?;
+            (StatusCode::OK, Some(serde_json::to_vec(&url_entry)?.into()))
+        }
+        &Method::GET => match req.uri().query() {
+            Some(k) => match store.get(k) {
+                Ok(link) => (StatusCode::OK, Some(link.into())),
+                Err(Error::NoSuchKey) => (StatusCode::NOT_FOUND, None),
+                Err(error) => return Err(error.into()),
+            },
+            None => {
+                let records: Vec<Link> = store
+                    .get_keys()
+                    .unwrap()
+                    .iter()
+                    .map(|k| store.get(k).unwrap())
+                    .map(|r| serde_json::from_slice(&r).unwrap())
+                    .collect();
+                (StatusCode::OK, Some(serde_json::to_vec(&records)?.into()))
+            }
+        },
+        &Method::DELETE => match req.uri().query() {
+            Some(k) => match store.delete(k) {
+                Ok(_) => (StatusCode::OK, None),
+                Err(error) => return Err(error.into()),
+            },
+            None => (StatusCode::NOT_FOUND, None),
+        },
+        _ => (StatusCode::METHOD_NOT_ALLOWED, None),
+    };
+
+    Ok(http::Response::builder().status(status).body(body)?)
+}
